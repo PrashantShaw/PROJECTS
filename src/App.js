1387aefeb917045176834f82e990c41cdd077ab9
PrashantShaw <prashant.shaw.3154@gmail.com>
2022-11-29 20:02:40 +0530

    tictactoe branch

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 // import TODO from "./components/TODO";
 // import {RoutingBase} from "./components/Routing/RoutingBase";
-// import TicTacToe from "./components/TicTacToe/IndexTicTacToe";
+import TicTacToe from "./components/TicTacToe/IndexTicTacToe";
 // import NEWS from "./components/NEWS WEBSITE/News";
-import CALCULATOR from "./components/Calculator/CALCULATOR";
+// import CALCULATOR from "./components/Calculator/CALCULATOR";
 // import SNAKE_GAME from "./components/SNAKE_GAME/SNAKE_GAME";
 
 
@@ -12,9 +12,9 @@ const App = function () {
   return <>
     {/* <TODO/> */}
     {/* <RoutingBase /> */}
-    {/* < TicTacToe /> */}
+    < TicTacToe />
     {/* < NEWS /> */}
-    <CALCULATOR />
+    {/* <CALCULATOR /> */}
     {/* <SNAKE_GAME /> */}
   </>
 
